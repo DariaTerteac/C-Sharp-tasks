23ec676d81942f48f6fab0d07358f268e0372069
Daria Terteac <dterteac@gmail.com>
2023-11-08 11:28:29 +0200

    Added Task #1

---
+++ Task-1/MatrixMultiplication.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace MatrixMultiplication
+{
+  class MatrixMultiplication
+  {
+    static void Main(string[] args)
+    {
+      int i, j, k;
+      int numberOfRows = 2;
+      int numberOfColumns = 2;
+      int[,] matrixA = { { 1, 2 }, { 3, 4 } };
+      int[,] matrixB = { { 5, 6 }, { 7, 8 } };
+      int[,] matrixC = new int[numberOfRows, numberOfColumns];
+
+      // Matrix multiplication
+      for (i = 0; i < numberOfRows; i++)
+      {
+        for (j = 0; j < numberOfColumns; j++)
+        {
+          matrixC[i, j] = 0;
+          for (k = 0; k < 2; k++)
+          {
+            matrixC[i, j] += matrixA[i, k] * matrixB[k, j];
+          }
+        }
+      }
+
+      // Display Matrix A
+      Console.WriteLine("Matrix A:");
+      for (i = 0; i < numberOfRows; i++)
+      {
+        for (j = 0; j < numberOfColumns; j++)
+        {
+          Console.Write(matrixA[i, j] + " ");
+        }
+        Console.WriteLine();
+      }
+
+      // Display Matrix B
+      Console.WriteLine("Matrix B:");
+      for (i = 0; i < numberOfRows; i++)
+      {
+        for (j = 0; j < numberOfColumns; j++)
+        {
+          Console.Write(matrixB[i, j] + " ");
+        }
+        Console.WriteLine();
+      }
+
+      // Display Matrix C
+      Console.WriteLine("Matrix C:");
+      for (i = 0; i < numberOfRows; i++)
+      {
+        for (j = 0; j < numberOfColumns; j++)
+        {
+          Console.Write(matrixC[i, j] + " ");
+        }
+        Console.WriteLine();
+      }
+    }
+  }
+}
