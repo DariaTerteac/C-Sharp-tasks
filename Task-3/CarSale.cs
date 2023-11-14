1c2268a69521f1bde3d95b426e8c89fc6784a08e
Daria Terteac <dterteac@gmail.com>
2023-11-14 15:00:12 +0200

    Updated CarSale program

--- Task-3/CarSale.cs
+++ Task-3/CarSale.cs
@@ -2,10 +2,37 @@
 
 class Car
 {
-    public int Year;
-    public double Price;
-    public string Color;
-    public string Condition;
+    public string Manufacturer { get; set; }
+    public int Year { get; set; }
+    public double Price { get; set; }
+    public string Color { get; set; }
+    public string Condition { get; set; }
+
+    public static void SortAndDisplayCars(Car[] cars)
+    {
+        // Sort cars by year and price
+        var sortedCars = cars.OrderBy(car => car.Year)
+                            .ThenByDescending(car => car.Price)
+                            .ToArray();
+
+        // Display sorted car list
+        foreach (Car car in sortedCars)
+            Console.WriteLine($"Car Information: Manufacturer {car.Manufacturer}, Year {car.Year}, Price ${car.Price}, Color {car.Color}, Condition {car.Condition}");
+    }
+
+    public static void FindAndDisplayBestCar(Car[] cars)
+    {
+        // Find the best car
+        var bestCar = cars.OrderByDescending(car => car.Year)
+                         .ThenByDescending(car => car.Price)
+                         .FirstOrDefault();
+
+        // Display information about the best car
+        if (bestCar != null)
+            Console.WriteLine($"Information about the Best Car: Manufacturer {bestCar.Manufacturer}, Year {bestCar.Year}, Price ${bestCar.Price}, Color {bestCar.Color}, Condition {bestCar.Condition}");
+        else
+            Console.WriteLine("No cars in the array.");
+    }
 }
 
 class CarSale
@@ -16,121 +43,73 @@ class CarSale
         Car[] cars = new Car[20];
 
         // Initializing the cars with some sample data
-        cars[0] = new Car { Year = 2020, Price = 25000.10, Color = "Red", Condition = "New" };
-        cars[1] = new Car { Year = 2018, Price = 18000.50, Color = "Blue", Condition = "Used" };
-        cars[2] = new Car { Year = 2005, Price = 24000.24, Color = "Black", Condition = "New" };
-        cars[3] = new Car { Year = 2006, Price = 21000.75, Color = "Green", Condition = "Used" };
-        cars[4] = new Car { Year = 2000, Price = 22000.90, Color = "Red", Condition = "Used" };
-        cars[5] = new Car { Year = 2023, Price = 45000.45, Color = "Black", Condition = "Used" };
-        cars[6] = new Car { Year = 2022, Price = 28000.75, Color = "Blue", Condition = "Used" };
-        cars[7] = new Car { Year = 2022, Price = 29000.35, Color = "White", Condition = "New" };
-        cars[8] = new Car { Year = 2023, Price = 66000.75, Color = "Red", Condition = "New" };
-        cars[9] = new Car { Year = 2020, Price = 23000.50, Color = "Black", Condition = "New" };
-        cars[10] = new Car { Year = 2016, Price = 43000.60, Color = "Blue", Condition = "New" };
-        cars[11] = new Car { Year = 2004, Price = 12000.75, Color = "White", Condition = "Used" };
-        cars[12] = new Car { Year = 2004, Price = 11500.80, Color = "Green", Condition = "Used" };
-        cars[13] = new Car { Year = 2007, Price = 16000.90, Color = "Red", Condition = "New" };
-        cars[14] = new Car { Year = 2008, Price = 14000.75, Color = "Green", Condition = "New" };
-        cars[15] = new Car { Year = 2013, Price = 25000.55, Color = "White", Condition = "Used" };
-        cars[16] = new Car { Year = 2015, Price = 37000.95, Color = "Red", Condition = "Used" };
-        cars[17] = new Car { Year = 2016, Price = 82000.75, Color = "Black", Condition = "New" };
-        cars[18] = new Car { Year = 2013, Price = 22000.85, Color = "Blue", Condition = "Used" };
-        cars[19] = new Car { Year = 2001, Price = 11000.15, Color = "White", Condition = "New" };
+        cars[0] = new Car { Manufacturer = "BYD", Year = 2020, Price = 25000.10, Color = "Red", Condition = "New" };
+        cars[1] = new Car { Manufacturer = "Geely", Year = 2018, Price = 18000.50, Color = "Blue", Condition = "Used" };
+        cars[2] = new Car { Manufacturer = "GWM", Year = 2005, Price = 24000.24, Color = "Black", Condition = "New" };
+        cars[3] = new Car { Manufacturer = "Changan", Year = 2006, Price = 21000.75, Color = "Green", Condition = "Used" };
+        cars[4] = new Car { Manufacturer = "Chery", Year = 2000, Price = 22000.90, Color = "Red", Condition = "Used" };
+        cars[5] = new Car { Manufacturer = "NIO", Year = 2023, Price = 45000.45, Color = "Black", Condition = "Used" };
+        cars[6] = new Car { Manufacturer = "Geely", Year = 2022, Price = 28000.75, Color = "Blue", Condition = "Used" };
+        cars[7] = new Car { Manufacturer = "Lynk & Co", Year = 2022, Price = 29000.35, Color = "White", Condition = "New" };
+        cars[8] = new Car { Manufacturer = "Haval", Year = 2023, Price = 66000.75, Color = "Red", Condition = "New" };
+        cars[9] = new Car { Manufacturer = "Haval", Year = 2020, Price = 23000.50, Color = "Black", Condition = "Used" };
+        cars[10] = new Car { Manufacturer = "SWM Motors", Year = 2016, Price = 43000.60, Color = "Blue", Condition = "New" };
+        cars[11] = new Car { Manufacturer = "JAC Motors", Year = 2004, Price = 12000.75, Color = "White", Condition = "Used" };
+        cars[12] = new Car { Manufacturer = "DFM", Year = 2004, Price = 11500.80, Color = "Green", Condition = "Used" };
+        cars[13] = new Car { Manufacturer = "SAIC Motor", Year = 2007, Price = 16000.90, Color = "Red", Condition = "New" };
+        cars[14] = new Car { Manufacturer = "Chery", Year = 2008, Price = 14000.75, Color = "Green", Condition = "New" };
+        cars[15] = new Car { Manufacturer = "BYD", Year = 2013, Price = 25000.55, Color = "White", Condition = "Used" };
+        cars[16] = new Car { Manufacturer = "GWM", Year = 2015, Price = 37000.95, Color = "Red", Condition = "Used" };
+        cars[17] = new Car { Manufacturer = "Geely", Year = 2016, Price = 82000.75, Color = "Black", Condition = "New" };
+        cars[18] = new Car { Manufacturer = "NIO", Year = 2013, Price = 22000.85, Color = "Blue", Condition = "Used" };
+        cars[19] = new Car { Manufacturer = "Geely", Year = 2001, Price = 11000.15, Color = "White", Condition = "New" };
 
-        var sortedCars = SortCarsByYearAndPrice(cars);
+        Car.SortAndDisplayCars(cars);
 
-        DisplayCarInformation(sortedCars);
+        Car.FindAndDisplayBestCar(cars);
 
-        Car bestCar = FindBestCar(cars);
-
-        DisplayBestCarInformation(bestCar);
-
-        var loanParams = GetLoanParameters();
-
-        var paymentAmount = CalculateMonthlyPayment(loanParams);
-
-        DisplayMonthlyPayment(paymentAmount);
+        CalculateLoan();
     }
 
-    // Sort cars by year and price
-    static Car[] SortCarsByYearAndPrice(Car[] cars)
+    // Get loan parameters
+    static (double fullPrice, double basePayment, int numberOfMonths) GetLoanParameters()
     {
-        return cars.OrderBy(car => car.Year)
-                    .ThenByDescending(car => car.Price)
-                    .ToArray();
+        double fullPrice, basePayment;
+        int numberOfMonths;
+
+        Console.Write("Enter the full price of the car: $");
+        if (!double.TryParse(Console.ReadLine(), out fullPrice))
+            Console.WriteLine("Invalid input for full price. Please enter a valid number.");
+
+        Console.Write("Enter the base payment: $");
+        if (!double.TryParse(Console.ReadLine(), out basePayment))
+            Console.WriteLine("Invalid input for base amount. Please enter a valid number.");
+
+        Console.Write("Enter the number of months for the loan: ");
+        if (!int.TryParse(Console.ReadLine(), out numberOfMonths))
+            Console.WriteLine("Invalid input for the number of months. Please enter a valid integer number.");
+
+        return (fullPrice, basePayment, numberOfMonths);
     }
 
-    // Display sorted car list
-    static void DisplayCarInformation(Car[] cars)
+    static void CalculateLoan()
     {
-        foreach (Car car in cars)
-        {
-            Console.WriteLine($"Car Information: Year {car.Year}, Price ${car.Price}, Color {car.Color}, Condition {car.Condition}");
-        }
+        // Get loan parameters from the user
+        (double fullPrice, double basePayment, int numberOfMonths) loanParams = GetLoanParameters();
+
+        // Calculate loan amount
+        double loanAmount = loanParams.fullPrice - loanParams.basePayment;
+
+        // Constants
+        const int monthsPerYear = 12;
+
+        // Calculate monthly payment 
+        double interestInPercent = 9;
+        var rateOfInterest = interestInPercent / monthsPerYear / 100; // Using the constant
+        var numberOfPayments = loanParams.numberOfMonths;
+        var monthlyPayment = (rateOfInterest * loanAmount) / (1 - Math.Pow(1 + rateOfInterest, numberOfPayments * -1));
+
+        // Display monthly payment
+        Console.WriteLine($"Monthly payment amount: {monthlyPayment:C}");
     }
-
-    // Find the best car
-    static Car FindBestCar(Car[] cars)
-    {
-        return cars.OrderByDescending(car => car.Year)
-                    .ThenByDescending(car => car.Price)
-                    .FirstOrDefault();
-    }
-
-    // Display information about the best car
-    static void DisplayBestCarInformation(Car bestCar)
-    {
-        if (bestCar != null)
-        {
-            Console.WriteLine($"Information about the Best Car: Year {bestCar.Year}, Price ${bestCar.Price}, Color {bestCar.Color}, Condition {bestCar.Condition}");
-        }
-        else
-        {
-            Console.WriteLine("No cars in the array.");
-        }
-    }
-
-    // Get loan parameters from the user
-    static (double, double, int) GetLoanParameters()
-    {
-        double loanAmount, interest;
-        int numberOfYears;
-
-        Console.Write("Enter the loan amount: $");
-        if (!double.TryParse(Console.ReadLine(), out loanAmount))
-        {
-            Console.WriteLine("Invalid input for loan amount. Please enter a valid number.");
-        }
-
-        Console.Write("Enter the interest rate (%): ");
-        if (!double.TryParse(Console.ReadLine(), out interest))
-        {
-            Console.WriteLine("Invalid input for interest rate. Please enter a valid number.");
-        }
-
-        Console.Write("Enter the number of years for the loan: ");
-        if (!int.TryParse(Console.ReadLine(), out numberOfYears))
-        {
-            Console.WriteLine("Invalid input for the number of years. Please enter a valid integer number.");
-        }
-
-        return (loanAmount, interest, numberOfYears);
-    }
-
-    // Calculate monthly payment 
-    static double CalculateMonthlyPayment((double loanAmount, double interest, int numberOfYears) loanParams)
-    {
-        var (loanAmount, interest, numberOfYears) = loanParams;
-        var rateOfInterest = interest / 1200;
-        var numberOfPayments = numberOfYears * 12;
-
-        return (rateOfInterest * loanAmount) / (1 - Math.Pow(1 + rateOfInterest, numberOfPayments * -1));
-    }
-
-    // Display monthly payment
-    static void DisplayMonthlyPayment(double paymentAmount)
-    {
-        Console.WriteLine($"Monthly payment amount: {paymentAmount:C}");
-    }
-
 }
