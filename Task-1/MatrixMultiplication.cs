9caedb50abd909be4abe76e9700c7c794bb84c6f
Daria Terteac <dterteac@gmail.com>
2023-11-14 19:27:40 +0200

    Updated MatrixMultiplication

--- Task-1/MatrixMultiplication.cs
+++ Task-1/MatrixMultiplication.cs
@@ -6,58 +6,40 @@ namespace MatrixMultiplication
   {
     static void Main(string[] args)
     {
-      int i, j, k;
       int numberOfRows = 2;
       int numberOfColumns = 2;
       int[,] matrixA = { { 1, 2 }, { 3, 4 } };
       int[,] matrixB = { { 5, 6 }, { 7, 8 } };
       int[,] matrixC = new int[numberOfRows, numberOfColumns];
 
-      // Matrix multiplication
-      for (i = 0; i < numberOfRows; i++)
+      // Method to display matrix
+      static void DisplayMatrix(string label, int[,] matrix)
       {
-        for (j = 0; j < numberOfColumns; j++)
+        Console.WriteLine(label);
+        foreach (int row in new[] { 0, 1 })
+        {
+          foreach (int col in new[] { 0, 1 })
+            Console.Write(matrix[row, col] + " ");
+          Console.WriteLine();
+        }
+      }
+
+      // Matrix multiplication
+      for (int i = 0; i < numberOfRows; i++)
+      {
+        for (int j = 0; j < numberOfColumns; j++)
         {
           matrixC[i, j] = 0;
-          for (k = 0; k < 2; k++)
-          {
+          foreach (int k in new[] { 0, 1 })
             matrixC[i, j] += matrixA[i, k] * matrixB[k, j];
-          }
         }
       }
 
-      // Display Matrix A
-      Console.WriteLine("Matrix A:");
-      for (i = 0; i < numberOfRows; i++)
-      {
-        for (j = 0; j < numberOfColumns; j++)
-        {
-          Console.Write(matrixA[i, j] + " ");
-        }
-        Console.WriteLine();
-      }
+      // Display matrices
+      DisplayMatrix("Matrix A:", matrixA);
+      DisplayMatrix("Matrix B:", matrixB);
+      DisplayMatrix("Matrix C:", matrixC);
 
-      // Display Matrix B
-      Console.WriteLine("Matrix B:");
-      for (i = 0; i < numberOfRows; i++)
-      {
-        for (j = 0; j < numberOfColumns; j++)
-        {
-          Console.Write(matrixB[i, j] + " ");
-        }
-        Console.WriteLine();
-      }
-
-      // Display Matrix C
-      Console.WriteLine("Matrix C:");
-      for (i = 0; i < numberOfRows; i++)
-      {
-        for (j = 0; j < numberOfColumns; j++)
-        {
-          Console.Write(matrixC[i, j] + " ");
-        }
-        Console.WriteLine();
-      }
     }
   }
 }
